c7b4f7859e45321223a53979378ea546125422b8
Lukas Pillibeit <lukas.pillibeit@innogames.com>
2023-09-28 13:41:18 +0200

    Write Validator for Product

---
+++ RaspbrrryFridge backend/src/main/java/com/example/raspbrrryfridge/domain/products/ProductValidator.java
@@ -0,0 +1,28 @@
+package com.example.raspbrrryfridge.domain.products;
+
+import com.example.raspbrrryfridge.system.conversion.ValidatorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductValidator implements ValidatorService<ProductDto> {
+    @Override
+    public Boolean isValid(ProductDto productDto) {
+        if(productDto == null){
+            return false;
+        }
+        if(productDto.name() == null || productDto.name().isEmpty()){
+            return false;
+        }
+        if(productDto.weight() <= 0){
+            return false;
+        }
+        if(productDto.url() == null || productDto.url().isEmpty()){
+            return false;
+        }
+        if(productDto.ean() == null){
+            return false;
+        }
+        return true;
+
+    }
+}
