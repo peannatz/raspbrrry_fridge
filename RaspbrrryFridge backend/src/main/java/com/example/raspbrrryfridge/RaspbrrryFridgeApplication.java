0d0e1f1e585f8a706efb68b610d00f98da7ad26e
Lukas Pillibeit <lukas.pillibeit@innogames.com>
2024-03-08 14:04:33 +0100

    Try to dockerize spring app

--- RaspbrrryFridge backend/src/main/java/com/example/raspbrrryfridge/RaspbrrryFridgeApplication.java
+++ RaspbrrryFridge backend/src/main/java/com/example/raspbrrryfridge/RaspbrrryFridgeApplication.java
@@ -1,6 +1,5 @@
 package com.example.raspbrrryfridge;
 
-import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
